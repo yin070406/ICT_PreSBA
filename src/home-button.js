dc3ed3d760bf7a3af5a40e96df002ce3d6c01d9f
yin070406 <64211270+yin070406@users.noreply.github.com>
2023-06-03 00:54:34 +0800

    Delete src directory

--- src/home-button.js
+++
@@ -1,5 +0,0 @@
-var buttonstart = document.getElementsByClassName("start")[0];
-
-buttonstart.addEventListener("click", function() {
-  window.location.href = "/file/introduction/index.html";
-});
--- src/text.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".js-texts", {
-    strings: ["Hello", "Bye", "Why are you here?"],
-    typeSpeed: 100,
-    backSpeed: 50,
-    loop: true,
-})
